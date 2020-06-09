9e7087d96ef2aaef256ed05e9c6c95c93cdb3346
Skyhnb <564318045@qq.com>
2020-06-09 08:47:57 +0800

    init

---
+++ eurekaservice/src/test/java/com/wxt/eurekaservice/EurekaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxt.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
